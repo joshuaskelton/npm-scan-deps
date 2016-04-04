31a2c4d070573a24fc57e1e7fcf3bd3ec536dcb1
Joshua Skelton <joskelto@microsoft.com>
2016-04-04 16:09:43 -0700

    :circus_tent: Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# npm-scan-deps
+
+## Getting Started
+
+`npm install -g`
+
+## Usage
+
+`npm-scan-deps some-package@1.0.0`
---
+++ npm-scan-deps.js
@@ -0,0 +1,54 @@
+var ls = require("npm-remote-ls").ls;
+var parseArgs = require("minimist");
+var argv = parseArgs(process.argv.slice(2), {alias: {v: "version", h: "help"}});
+var securityHoldingPackages = require("./securityHoldingPackages.json").packages;
+
+if (argv.version) {
+    var version = require("./package.json").version;
+    console.log(version);
+    process.exit(0);
+}
+
+var showUsage = function() {
+        console.log(
+`Usage: npm-scan-deps <package>
+
+Options:
+  -h, --help
+  -v, --version
+`
+    );
+};
+
+if (argv.help || argv._.length === 0) {
+    showUsage();
+    process.exit(0);
+}
+
+var npmPackageRegex = /([\w-]+)(?:@(.*))?/;
+var matches = npmPackageRegex.exec(argv._[0]);
+var packageName = matches[1];
+var packageVersion = matches[2] || "latest";
+var foundPackages = [];
+
+ls(packageName, packageVersion, true, function(dependencies) {
+    dependencies.forEach(function(dependencyName) {
+        var matches = npmPackageRegex.exec(dependencyName);
+        var name = matches[1];
+
+        if (securityHoldingPackages.indexOf(name) >= 0) {
+            if (foundPackages.indexOf(name) === -1) {
+                foundPackages.push(name);
+            }
+        }
+    });
+
+    console.log(`Scanned: ${dependencies.length} dependencies`);
+    console.log(`Found: ${foundPackages.length} issue(s)`);
+
+    foundPackages.forEach(function(packageName) {
+        console.log(`  ${foundPackages.indexOf(packageName) + 1}. ${packageName}`);
+    });
+
+    console.log("");
+});
